19acbef9bdacf26a752682e2921625f8e5c38082
Brett Settle <brettjsettle@gmail.com>
2017-08-20 21:31:34 -0700

    build testing

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 set -e
-git checkout build
-rm -rf `ls --ignore=node_modules`
-git checkout master
 npm install
 npm run build
 git checkout build
-mv build/* .
-rmdir build
+cp -r build/* .
+rm -rf build
